3c0c01b9dcf6809d9b9e10cf6bb11f99e2b3cf51
Nicholas Morey <nicholas@morey.tech>
2023-10-17 11:28:22 -0400

    fix: move app to bootstrap folder
    Signed-off-by: Nicholas Morey <nicholas@morey.tech>

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ locals {
   cluster_name         = "gitops-bridge-gke"
   environment          = "dev"
   addons_repo_url      = "https://github.com/morey-tech/gitops-bridge-control-plane"
-  addons_repo_path     = "addons"
+  addons_repo_path     = "bootstrap"
   addons_repo_revision = "HEAD"
 
   # Each entry must have a corresponding ApplicationSet in the `addons_repo`.
